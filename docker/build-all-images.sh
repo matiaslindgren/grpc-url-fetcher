9f018b67557b5c5688192bbc2bab589470400a20
Matias Lindgren <matias.lindgren@gmail.com>
2020-05-24 21:50:39 +0300

    add examples in build script

--- docker/build-all-images.sh
+++ docker/build-all-images.sh
@@ -1,3 +1,8 @@
-docker build --no-cache --tag urlfetcher:v1 --file docker/urlfetcher.dockerfile .
-docker build --no-cache --tag test-runner:v1 --file docker/test-runner.dockerfile .
-docker build --no-cache --tag http-echo-server:v1 --file docker/http-echo-server.dockerfile .
+# grpc-url-fetcher binaries
+docker build --tag urlfetcher:v1 --file docker/urlfetcher.dockerfile .
+# Tests
+docker build --tag test-runner:v1 --file docker/test-runner.dockerfile .
+docker build --tag http-echo-server:v1 --file docker/http-echo-server.dockerfile .
+# CLI example
+docker build --tag urlfetcher-client:v1 --file docker/urlfetcher-client.dockerfile .
+docker build --tag urlfetcher-server:v1 --file docker/urlfetcher-server.dockerfile .
